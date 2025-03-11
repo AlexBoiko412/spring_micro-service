351a345ce4284eba1a48e6de76b09183f75589e8
Alex <bojkosasa78@gmail.com>
2025-03-11 20:33:12 +0100

    Patient Repository

---
+++ patient-service/src/main/java/com/pt/patientservice/service/PatientService.java
@@ -0,0 +1,26 @@
+package com.pt.patientservice.service;
+
+import com.pt.patientservice.dto.PatientResponseDTO;
+import com.pt.patientservice.mapper.PatientMapper;
+import com.pt.patientservice.model.Patient;
+import com.pt.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients() {
+        List<Patient> patients = patientRepository.findAll();
+        return patients
+                .stream().map(PatientMapper::toDto).toList();
+    }
+}
