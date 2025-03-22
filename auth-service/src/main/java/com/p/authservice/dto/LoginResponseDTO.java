a0008fc7e95fd71ecb21902ef9bc45ca1f1b3a38
Alex <bojkosasa78@gmail.com>
2025-03-22 21:09:37 +0100

    Login Response DTO

---
+++ auth-service/src/main/java/com/p/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,13 @@
+package com.p.authservice.dto;
+
+public class LoginResponseDTO {
+    private String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
