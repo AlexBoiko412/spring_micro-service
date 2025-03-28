bc552f0aa5c179a8e59698c94e85588876ac3071
Alex <bojkosasa78@gmail.com>
2025-03-28 20:39:27 +0100

    Get auth token test

---
+++ integration-tests/src/test/java/AuthIntegrationTest.java
@@ -0,0 +1,34 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class AuthIntegrationTest {
+    @BeforeAll
+    static void setUpBeforeClass(){
+        RestAssured.baseURI = "http://localhost:8084";
+
+    }
+    @Test
+    public void shouldReturnOKWithValidToken() {
+        String loginPayload = """
+                    {
+                      "email": "john.doe@example.com",
+                      "password": "password123"
+                    }   
+                """;
+
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .body("token", Matchers.notNullValue())
+                .extract()
+                .response();
+        System.out.println("Generated token: \n" + response.getBody().prettyPrint());
+    }
+}
